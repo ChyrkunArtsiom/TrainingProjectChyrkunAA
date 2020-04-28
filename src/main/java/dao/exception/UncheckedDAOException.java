e5b509601b631e62cdf2a818d7b95ff143662537
Artsiom Chyrkun <chyrkun.artsiom@google.com>
2020-04-28 17:10:42 +0300

    Handling few exceptions

---
+++ src/main/java/dao/exception/UncheckedDAOException.java
@@ -0,0 +1,11 @@
+package dao.exception;
+
+public class UncheckedDAOException extends RuntimeException {
+    public UncheckedDAOException(String message){
+        super(message);
+    }
+
+    public UncheckedDAOException(String message, Throwable cause){
+        super(message, cause);
+    }
+}
